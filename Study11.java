939fd35d727f4e7a4d6f9503a4f36e6d01842db7
Junmo Park <jmjm9797@naver.com>
2021-11-22 18:16:34 +0900

    21.11.22 공부내용

---
+++ Study11.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Study11 {
+
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("수를 입력하시오 >");
+//		
+//		int i = sc.nextInt();
+//		
+//		
+//		long c = (long)i;
+//		
+//		System.out.println((double)i);
+//		
+//		System.out.printf("%d\n", i);
+//		System.out.printf("%d", c);
+//		System.out.println(i + "");
+		
+		char ch = (char)122;
+		System.out.println(ch);
+		
+		int in = (int)'['/']';
+		System.out.println(in);
+		
+		
+	}
+}
---
+++ Study12.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Study12 
+{
+
+	public static void main(String[] args) 
+	{
+//		int[] score = new int[5];
+//		
+//		score[4] = 100;
+//		
+//		System.out.println("score[0] = " + score[0]);
+//		System.out.println("score[1] = " + score[1]);
+//		System.out.println("score[2] = " + score[2]);
+//		System.out.println("score[3] = " + score[3]);
+//		System.out.println("score[4] = " + score[4]);
+//		
+//		int value = score[4];
+//		System.out.println(value);
+		
+		
+			int[] arr = new int[10];
+		System.out.println("arr.length="+ arr.length);
+		
+		for(int i =0; i < arr.length; ++i)
+		{
+			System.out.println("arr["+i+"]=" +arr[i]);
+		}
+		
+	}
+}
